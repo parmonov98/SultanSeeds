f98a0d39eebbf37810f77b7016c2d93fbc630f5e
joxaDev <jaxongirtoshpolatov0311@gmail.com>
2024-08-30 22:42:46 +0500

    made responsive for the phone

--- js/main.js
+++ js/main.js
@@ -1,87 +1,54 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const langSwitcher = document.querySelector(".header__language-switcher");
-  const langButton = document.querySelector(".header__language-button");
-  const langOptions = document.querySelector(".header__language-options");
-  const langOptionButtons = document.querySelectorAll(
-    ".header__language-option"
-  );
-  const selectedLangFlag = document.getElementById("selected-lang-flag");
-  const selectedLangText = document.getElementById("selected-lang-text");
+  const langSwitcher = document.querySelector(".header__language-switcher"),
+        langButton = document.querySelector(".header__language-button"),
+        langOptions = document.querySelector(".header__language-options"),
+        langOptionButtons = document.querySelectorAll(".header__language-option"),
+        selectedLangFlag = document.getElementById("selected-lang-flag"),
+        selectedLangText = document.getElementById("selected-lang-text"),
+        selectMenu = document.querySelector(".select-menu"),
+        selectButton = selectMenu.querySelector(".select-menu__button"),
+        options = selectMenu.querySelectorAll(".select-menu__option"),
+        selectButtonText = selectMenu.querySelector(".select-menu__button-text"),
+        menuIcon = document.querySelector(".hamburger");
 
-  let selectedLang = "en"; // Default selected language
+  let selectedLang = "en";
 
-  // Til almashtirgichni ochish/yopish
   langButton.addEventListener("click", () => {
     langSwitcher.classList.toggle("active");
-    langOptions.style.display = langSwitcher.classList.contains("active")
-      ? "block"
-      : "none";
+    langOptions.style.display = langSwitcher.classList.contains("active") ? "block" : "none";
   });
 
-  // Tilni tanlash
-  langOptionButtons.forEach((button) => {
+  langOptionButtons.forEach(button => {
     button.addEventListener("click", () => {
-      const lang = button.dataset.lang;
-
-      // Tanlangan tilni yangilash
-      selectedLang = lang;
+      selectedLang = button.dataset.lang;
       updateSelectedLang();
-
-      // Til almashtirgichni yopish
       langSwitcher.classList.remove("active");
       langOptions.style.display = "none";
     });
   });
 
   function updateSelectedLang() {
-    langOptionButtons.forEach((button) => {
-      if (button.dataset.lang === selectedLang) {
-        button.classList.add("selected");
-        button.style.display = "none";
-      } else {
-        button.classList.remove("selected");
-        button.style.display = "block";
-      }
+    langOptionButtons.forEach(button => {
+      button.classList.toggle("selected", button.dataset.lang === selectedLang);
+      button.style.display = button.dataset.lang === selectedLang ? "none" : "block";
     });
-
-    // Yangilangan tilni switcherda ko'rsatish
-    if (selectedLang === "en") {
-      selectedLangFlag.src = "./photo/icon/GB.png";
-      selectedLangText.textContent = "English";
-    } else if (selectedLang === "ru") {
-      selectedLangFlag.src = "./photo/icon/RU.png";
-      selectedLangText.textContent = "Russian";
-    }
+    selectedLangFlag.src = selectedLang === "en" ? "./photo/icon/GB.png" : "./photo/icon/RU.png";
+    selectedLangText.textContent = selectedLang === "en" ? "English" : "Russian";
   }
 
-  // Dastlabki tanlangan tilni yangilash
   updateSelectedLang();
-
-  //Multiple options dropdown
-  //https://codepen.io/gatoledo1/pen/QWmpWjK
-
-  const selectMenu = document.querySelector(".select-menu"),
-    selectButton = selectMenu.querySelector(".select-menu__button"),
-    options = selectMenu.querySelectorAll(".select-menu__option"),
-    selectButtonText = selectMenu.querySelector(".select-menu__button-text");
 
   selectButton.addEventListener("click", () => {
     selectMenu.classList.toggle("select-menu--active");
   });
 
-  options.forEach((option) => {
+  options.forEach(option => {
     option.addEventListener("click", () => {
-      let selectedOption = option.querySelector(
-        ".select-menu__option-text"
-      ).innerText;
-      selectButtonText.innerText = selectedOption;
-
+      selectButtonText.innerText = option.querySelector(".select-menu__option-text").innerText;
       selectMenu.classList.remove("select-menu--active");
     });
   });
 
-  // open menu
-  const menuIcon = document.querySelector(".hamburger");
   menuIcon.addEventListener("click", () => {
     document.querySelector("header").classList.toggle("active");
     document.querySelector("#hamburger").classList.toggle("active");
