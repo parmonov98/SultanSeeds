523db3a703cab1d7b21d6563c493cb2f85791c66
joxaDev <jaxongirtoshpolatov0311@gmail.com>
2024-09-12 08:04:51 +0500

    nothing

--- js/initLanguageSwitcher.js
+++ js/initLanguageSwitcher.js
@@ -1,1 +1,48 @@
-export function initLanguageSwitcher({switcherSelector:e,buttonSelector:t,optionsSelector:n,optionSelector:l,flagSelector:a,textSelector:c,flagImages:s={en:"./photo/icon/GB.png",ru:"./photo/icon/RU.png"},texts:o={en:"English",ru:"Russian"}}){let i=document.querySelector(e),r=document.querySelector(t),u=document.querySelector(n),g=document.querySelectorAll(l),y=document.querySelector(a),d=document.querySelector(c),p=window.location.pathname,h=p.split("/"),L=h[1],S=L;function q(){g.forEach(e=>{e.classList.toggle("selected",e.dataset.lang===S),e.style.display=e.dataset.lang===S?"none":"block"}),y.src=s[S],d.textContent=o[S]}"ru"===L?(o.ru="Русский",o.en="Английский",S="ru"):S="en",r.addEventListener("click",()=>{i.classList.toggle("active"),u.style.display=i.classList.contains("active")?"block":"none"}),g.forEach(e=>{e.addEventListener("click",()=>{S=e.dataset.lang,q(),i.classList.remove("active"),u.style.display="none"})}),q()}
+export function initLanguageSwitcher({
+  switcherSelector: e,
+  buttonSelector: t,
+  optionsSelector: n,
+  optionSelector: l,
+  flagSelector: a,
+  textSelector: c,
+  flagImages: s = {
+    en: "https://ik.imagekit.io/testcloud/SultanSeeds/photo/icon/GB.png?updatedAt=1725041170928",
+    ru: "https://ik.imagekit.io/testcloud/SultanSeeds/photo/icon/RU.png?updatedAt=1725041169328",
+  },
+  texts: o = { en: "English", ru: "Russian" },
+}) {
+  let i = document.querySelector(e),
+    r = document.querySelector(t),
+    u = document.querySelector(n),
+    g = document.querySelectorAll(l),
+    y = document.querySelector(a),
+    d = document.querySelector(c),
+    p = window.location.pathname,
+    h = p.split("/"),
+    L = h[1],
+    S = L;
+  function q() {
+    g.forEach((e) => {
+      e.classList.toggle("selected", e.dataset.lang === S),
+        (e.style.display = e.dataset.lang === S ? "none" : "block");
+    }),
+      (y.src = s[S]),
+      (d.textContent = o[S]);
+  }
+  "ru" === L
+    ? ((o.ru = "Русский"), (o.en = "Английский"), (S = "ru"))
+    : (S = "en"),
+    r.addEventListener("click", () => {
+      i.classList.toggle("active"),
+        (u.style.display = i.classList.contains("active") ? "block" : "none");
+    }),
+    g.forEach((e) => {
+      e.addEventListener("click", () => {
+        (S = e.dataset.lang),
+          q(),
+          i.classList.remove("active"),
+          (u.style.display = "none");
+      });
+    }),
+    q();
+}
