c41c8df8d1dcabdbd5641eb392bed2c29d97de49
joxaDev <jaxongirtoshpolatov0311@gmail.com>
2024-09-17 15:42:01 +0500

    js file path replace

--- js/sendMessage.js
+++ js/sendMessage.js
@@ -4,9 +4,18 @@ const botToken = "7059505881:AAHlU_yTsaa6_XwS9yg7MCmLjIv8rsoKEhE";
 // Guruh chat ID sini bu yerga qo'ying (masalan, -1001234567890)
 const chatId = "-1002477600463";
 
+// malumotlarni yig'ish
+const userEmail = document.querySelectorAll(".order-form .form-emil");
+const userInterested = document.querySelectorAll(
+  ".order-form .select-menu__button-text"
+);
+const userPhoneNumber = document.querySelectorAll(
+  ".order-form .form-phone input"
+);
+const userMessage = document.querySelectorAll(".order-form #message");
+
 // Yuboriladigan xabar
-const message =
-  "Matn noto'g'ri o'zgaruvchiga biriktirilgan bo'lishi mumkin. Quyidagi kodning butunligini qayta ko'rib chiqing:";
+const message = `Yangi xabar! Company email: ${userEmail}, /n Company phone: ${userPhoneNumber}, /n Company message: ${userMessage}`;
 
 // Xabarni yuborish uchun fetch API dan foydalanish
 function sendMessageToTelegram() {
@@ -33,5 +42,10 @@ function sendMessageToTelegram() {
     });
 }
 
+const sendMessage = document.querySelector(".order-form button");
+
+sendMessage.addEventListener("click", () => {
+  sendMessageToTelegram();
+});
 // Xabar yuborishni chaqirish
-sendMessageToTelegram();
+// sendMessageToTelegram();
