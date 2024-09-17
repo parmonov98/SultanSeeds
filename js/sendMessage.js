d4d7e0dc7892c0c93a40201a0a5c606a0336a61d
joxaDev <jaxongirtoshpolatov0311@gmail.com>
2024-09-17 15:16:48 +0500

    js file path replace

--- js/sendMessage.js
+++ js/sendMessage.js
@@ -1,34 +1,35 @@
 // Bot tokeningizni bu yerga joylashtiring
-const botToken = '7059505881:AAHlU_yTsaa6_XwS9yg7MCmLjIv8rsoKEhE';
+const botToken = "7059505881:AAHlU_yTsaa6_XwS9yg7MCmLjIv8rsoKEhE";
 
 // Guruh chat ID sini bu yerga qo'ying (masalan, -1001234567890)
-const chatId = '-1002477600463';
+const chatId = "-1002477600463";
 
 // Yuboriladigan xabar
-const message = 'Salom, bu sinov xabari!';
+const message =
+  "Matn noto'g'ri o'zgaruvchiga biriktirilgan bo'lishi mumkin. Quyidagi kodning butunligini qayta ko'rib chiqing:";
 
 // Xabarni yuborish uchun fetch API dan foydalanish
 function sendMessageToTelegram() {
-    fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            chat_id: chatId,
-            text: message
-        })
+  fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      chat_id: chatId,
+      text: message,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.ok) {
+        console.log("Xabar muvaffaqiyatli yuborildi:", data);
+      } else {
+        console.error("Xatolik yuz berdi:", data);
+      }
     })
-    .then(response => response.json())
-    .then(data => {
-        if (data.ok) {
-            console.log('Xabar muvaffaqiyatli yuborildi:', data);
-        } else {
-            console.error('Xatolik yuz berdi:', data);
-        }
-    })
-    .catch(error => {
-        console.error('Fetch API xatosi:', error);
+    .catch((error) => {
+      console.error("Fetch API xatosi:", error);
     });
 }
 
