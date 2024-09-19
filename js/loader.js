4a150c12050e3d118265a5aa0f58a8225ee6e945
parmonov98 <parmonov98@yandex.ru>
2024-09-20 00:47:31 +0500

    videoloaded added

--- js/loader.js
+++ js/loader.js
@@ -2,13 +2,26 @@ document.addEventListener('DOMContentLoaded', () => {
   // Select the content container
   var content = document.querySelector('body')
 
+  let is_img = false;
+  let is_video = false;
   // Use imagesLoaded to wait for all images within 'content' to load
   imagesLoaded(content, function() {
     console.log('All images have loaded.');
-      document.body.classList.remove("no-scroll");
-      setTimeout(() => {
-        document.querySelector(".page-loader")?.classList.add('hide')
-      }, 0)
-    // Proceed to check other assets
+      is_img = true;
   });
+
+  
+  // Use imagesLoaded to wait for all images within 'content' to load
+  new VideosLoaded(content, function() {
+    console.log('All videos have loaded.');
+      is_video = true;
+  });
+
+  
+  setInterval(() => {
+    if (is_img && is_video) {
+      document.body.classList.remove("no-scroll");
+      document.querySelector(".page-loader")?.classList.add('hide')      
+    }
+  }, 100)
 })
