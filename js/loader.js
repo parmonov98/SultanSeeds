885bdca5368b78d4b5299bac69ac13ca9979800c
parmonov98 <parmonov98@yandex.ru>
2024-09-20 00:33:50 +0500

    loader issue fixed

---
+++ js/loader.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Select the content container
+  var content = document.querySelector('body')
+
+  // Use imagesLoaded to wait for all images within 'content' to load
+  imagesLoaded(content, function() {
+    console.log('All images have loaded.');
+      document.body.classList.remove("no-scroll");
+      setTimeout(() => {
+        document.querySelector(".page-loader")?.classList.add('hide')
+      }, 1000)
+    // Proceed to check other assets
+  });
+})
